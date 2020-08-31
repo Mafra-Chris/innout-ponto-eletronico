877026e0de69a6899b24ae50069e332fb896c6f1
Christiano <mafradesouza.cs@gmail.com>
2020-08-31 09:57:59 -0300

    header / footer

---
+++ innout/src/views/day_records.php
@@ -0,0 +1,1 @@
+<h1>TESTE DAY RECORDS</h1>
---
+++ innout/src/views/template/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <span>Desenvolvido </span>
+    <!--<span><i class="icofont-heart text-danger mx-1"></i></span>-->
+    <span>por Chris</span>
+</footer>
+
+</body>
+</html>
