46447723aad45be730528d87e3aece9a7bd2c2fb
Christiano <mafradesouza.cs@gmail.com>
2020-08-31 21:06:57 -0300

    sessao

--- innout/public/index.php
+++ innout/public/index.php
@@ -5,7 +5,7 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 $uri = urldecode($_SERVER['REQUEST_URI']);
 
 if($uri === '/' || $uri === '' || $uri === '/index.php'){
-    $uri = '/login.php';
+    $uri = '/day_records.php';
 }
 
 require_once(CONTROLLER_PATH . "/{$uri}");
